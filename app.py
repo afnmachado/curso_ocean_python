fd6eb7e1830e2f64b60e13fa5a81bb6d1f9be7e1
afnmachado <lipemach@yahoo.com.br>
2021-08-26 22:39:10 +0000

    alteracoes do dia 3 em app e entradas

--- app.py
+++ app.py
@@ -20,10 +20,28 @@ def depois_request(exc):
     g.bd.close()
 
 @app.route('/')
+@app.route('/entradas')
 def exibir_entradas():
     #return "<h1>Aqui estarão as postagens!!</h1>"
-    return render_template('exibir_entradas.html')
+    #return render_template('exibir_entradas.html', mensagem="Olá pessoas!", img="https://image.freepik.com/fotos-gratis/imagem-aproximada-em-tons-de-cinza-de-uma-aguia-careca-americana-em-um-fundo-escuro_181624-31795.jpg")
+    sql = "SELECT titulo, texto FROM entradas order by id DESC"
+    cur = g.bd.execute(sql)
+    entradas = []
+    for titulo, texto in cur.fetchall():
+        entradas.append({'titulo': titulo, 'texto': texto})
+    return render_template('exibir_entradas.html', entradas=entradas)
 
-@app.route('/hello')
-def pagina_inicial():
-    return "Hello World"
+#@app.route('/hello')
+#def pagina_inicial():
+#    return "Hello World"
+
+@app.route('/inserir')
+def inserir_entrada():
+    if not session.get('logado')
+        abort(401)
+    sql = "INSERT INTO entradas(titulo, texto) VALUES (?,?)"
+    d.bd.execute(sql, request.form['campoTitulo'], request.form['campoTexto'],)
+    #sql = "INSERT INTO entradas(titulo, texto) VALUES ('Segundo Post','Esse é o segundo post')"
+    #g.bd.execute(sql)
+    #g.bd.commit()
+    return redirect('/entradas')
